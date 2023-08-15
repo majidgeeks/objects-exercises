3f24b2e913379dce029fa1588e8e404c717f3d2a
majidgeeks <majidrajput246@gmail.com>
2023-08-16 02:14:54 +0500

    objects exercises

---
+++ app.js
@@ -0,0 +1,134 @@
+//student name
+//roll number
+//subject 1
+//subject 2
+
+//   var student = {
+//   name1 : prompt("type your name"),
+//   rollNumber : prompt("type your roll number"),
+//   subject1 : prompt("type subject1"),
+//   subject2 : prompt("type subject2"),
+
+// }
+
+// var studentData = [rollNumber, subject1, subject2 ];
+
+// console.log(studentData);  
+
+
+// 1. Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// Sample Output: name,sclass,rollno
+
+
+//  var student = {
+//  name : "David Rayy",
+//  sclass : "VI",
+//  rollno : 12 
+//                };
+
+// document.write(student.name+ "<br>");               
+// document.write(student.sclass+ "<br>");
+// document.write(student.rollno);
+
+
+// 3. Write a JavaScript program to get the length of a JavaScript object. Go to the editor
+// Sample object :
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+// console.log(student.length);
+//?????????????????????????????
+
+
+
+// 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+// Sample object:
+
+
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+// console.log(student)
+// delete student.rollno;
+// console.log(student);
+
+
+
+// 3. Write a JavaScript program to get the length of a JavaScript object.
+// Sample object :
+
+
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+
+// 4. Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books.
+
+// var library = [ 
+//    {
+//        author: 'Bill Gates',
+//        title: 'The Road Ahead',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Steve Jobs',
+//        title: 'Walter Isaacson',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Suzanne Collins',
+//        title:  'Mockingjay: The Final Book of The Hunger Games', 
+//        readingStatus: false
+//    }];
+
+
+// solution
+
+// var library = [ 
+//        {
+//            author: 'Bill Gates',
+//            title: 'The Road Ahead',
+//            readingStatus: true
+//        },
+//        {
+//            author: 'Steve Jobs',
+//            title: 'Walter Isaacson',
+//            readingStatus: true
+//        },
+//        {
+//            author: 'Suzanne Collins',
+//            title:  'Mockingjay: The Final Book of The Hunger Games', 
+//            readingStatus: false
+//        }];
+
+//        for(var i = 0; i < library.length; i++){
+//         var book = "'" + library[i].title + "'" + " by " + "'" + library[i].author + "'";
+//           if(library[i].readingStatus){
+//             console.log(book + "you have already read");
+//           }
+//           else{
+//             console.log("you still need to read this " + book);
+//           }
+
+//        };
+
+
+
+
